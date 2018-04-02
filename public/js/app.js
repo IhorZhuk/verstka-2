0140b68d6b641f6137ec7e1e8daa92965bfd6b61
IhorZhuk <ihor.zhuk@wazzupa.com>
2018-04-02 08:23:48 +0200

    add page slider

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,35 @@
-"use strict";
+'use strict';
 
-var x = 3;
-console.log(x);
+var initPageSlider = function initPageSlider() {
+
+  var $slider = $('.js-page-slider');
+
+  $slider.slick({
+    arrows: false,
+    vertical: true
+  });
+
+  $slider.mousewheel(function (e) {
+    e.preventDefault();
+    if (e.deltaY < 0) {
+      $slider.slick('slickNext');
+    } else {
+      $slider.slick('slickPrev');
+    }
+  });
+
+  $slider.swipe({
+    swipe: function swipe(e, direction, distance, duration, fingerCount, fingerData) {
+      if (direction == 'up') {
+        $slider.slick('slickNext');
+      } else {
+        $slider.slick('slickPrev');
+      }
+    },
+    threshold: 50
+  });
+};
+
+$(document).ready(function () {
+  initPageSlider();
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,34 @@
-const x = 3;
-console.log(x);
+const initPageSlider = () => {
+
+  const $slider = $('.js-page-slider');
+
+  $slider.slick({
+    arrows: false,
+    vertical: true
+  });
+
+  $slider.mousewheel((e) => {
+    e.preventDefault();
+    if (e.deltaY < 0) {
+      $slider.slick('slickNext');
+    } else {
+      $slider.slick('slickPrev');
+    }
+  });
+
+  $slider.swipe({
+    swipe: (e, direction, distance, duration, fingerCount, fingerData) => {
+      if (direction == 'up') {
+        $slider.slick('slickNext');
+      } else {
+        $slider.slick('slickPrev');
+      }
+    },
+    threshold: 50
+  });
+
+}
+
+$(document).ready(()=> {
+  initPageSlider();
+})
