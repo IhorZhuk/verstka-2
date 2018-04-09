46ef6a11eea2e20ce14649da259f5af92e1f87d9
IhorZhuk <ihor.zhuk@wazzupa.com>
2018-04-09 10:58:08 +0200

    add page 2. Css fixes for page 1

--- public/js/app.js
+++ public/js/app.js
@@ -30,6 +30,26 @@ var initPageSlider = function initPageSlider() {
   });
 };
 
+var initWorkSlider = function initWorkSlider() {
+  var $wrapper = $('.js-work-slider'),
+      $slide1 = $wrapper.find('.js-slide-1').html(),
+      $slide2 = $wrapper.find('.js-slide-2').html(),
+      $slide3 = $wrapper.find('.js-slide-3').html();
+
+  $wrapper.html(' ');
+
+  $('<div>').appendTo($wrapper).append($slide1).append($slide2).append($slide3);
+  $('<div>').appendTo($wrapper).append($slide2).append($slide3).append($slide1);
+  $('<div>').appendTo($wrapper).append($slide3).append($slide1).append($slide2);
+
+  $('.js-work-slider').slick({
+    fade: true,
+    prevArrow: '<span class="work-arrow is-prev">\n                  <img src="img/arrow-left.svg" width="18" height="14">\n                </span>',
+    nextArrow: '<span class="work-arrow is-next">\n                  <img src="img/arrow-right.svg" width="18" height="14">\n                </span>'
+  });
+};
+
 $(document).ready(function () {
   initPageSlider();
+  initWorkSlider();
 });
--- src/js/app.js
+++ src/js/app.js
@@ -29,6 +29,31 @@ const initPageSlider = () => {
 
 }
 
+const initWorkSlider = () => {
+  const $wrapper = $('.js-work-slider'),
+        $slide1= $wrapper.find('.js-slide-1').html(),
+        $slide2= $wrapper.find('.js-slide-2').html(),
+        $slide3= $wrapper.find('.js-slide-3').html();
+
+  $wrapper.html(' ');
+
+  $('<div>').appendTo($wrapper).append($slide1).append($slide2).append($slide3);
+  $('<div>').appendTo($wrapper).append($slide2).append($slide3).append($slide1);
+  $('<div>').appendTo($wrapper).append($slide3).append($slide1).append($slide2);
+
+  $('.js-work-slider').slick({
+    fade: true,
+    prevArrow: `<span class="work-arrow is-prev">
+                  <img src="img/arrow-left.svg" width="18" height="14">
+                </span>`,
+    nextArrow: `<span class="work-arrow is-next">
+                  <img src="img/arrow-right.svg" width="18" height="14">
+                </span>`,
+  })
+}
+
+
 $(document).ready(()=> {
   initPageSlider();
+  initWorkSlider();
 })
